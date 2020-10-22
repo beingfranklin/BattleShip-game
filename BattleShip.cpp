7105afa56f483b6c0a463559d1f5d3eebedae93c
Franklin Antony <franklin@Franklins-MacBook-Pro-2.local>
2020-10-22 01:57:04 +0100

    Functions week 4 wrapped up

--- BattleShip.cpp
+++ BattleShip.cpp
@@ -6,40 +6,36 @@
 using namespace std;
 
 // Added funtion protoype to reuse the printMenu()
-void printMenu();
+void printMainMenu();
 
-void beginGame()
+void getPlayerName(char &playerName)
 {
-    char playerName, X, Y;
     cout << "\nEnter player name:\n";
     cin >> playerName;
-    getCoordinates(X,Y);
-    cout << "\nYou have entered the coordinates X: " << X << " and Y:" << Y << "\n";
 }
 
-void exitOut()
+bool inValidDirection(char direction)
 {
-    cout << "\nGood Bye!!!\n";
-    exit(0);
-}
-
-void endGame()
-{
-    char response;
-    cout << "\n Are you sure you want to exit the game? (Y/N)\n";
-    cin >> response;
-    switch (response)
+    if (!(
+            direction == 'N' || direction == 'S' || direction == 'W' || direction == 'E' ||
+            direction == 'n' || direction == 's' || direction == 'w' || direction == 'e'))
     {
-    case 'Y':
-    case 'y':
-        exitOut();
-        break;
-    case 'N':
-    case 'n':
-    default:
-        printMenu();
-        break;
+        return true;
     }
+    return false;
+}
+
+void getDirection(char &direction)
+{
+    do
+    {
+        if (inValidDirection(direction) && direction != 'z')
+        {
+            cout << "Invalid direction!\n";
+        }
+        cout << "\nEnter the direction(N/E/S/W)";
+        cin >> direction;
+    } while (inValidDirection(direction));
 }
 
 void getCoordinates(char &X, char &Y)
@@ -66,6 +62,91 @@ void getCoordinates(char &X, char &Y)
     } while (!(Y >= '1' && Y <= '8'));
 }
 
+void addShips(int &count)
+{
+    char X, Y, direction = 'z';
+    cout << "\n----- Adding Ship " << count + 1 << " of 5 ----- ";
+    getCoordinates(X, Y);
+    getDirection(direction);
+    cout << "\nYou have added a ship to the coordinates X: " << X << " and Y:" << Y << " in " << direction << " direction\n\n";
+    count++;
+}
+void enterToContinue()
+{
+    cout << "\nPress enter to continue!";
+    cin.clear();          //clears the stream
+    cin.ignore(50, '\n'); //ignores next line(enter)
+    cin.get();            //“cin>>” expects at least one char input, just enter doesn’t work
+}
+
+void displayBoardState(int fireCount)
+{
+    int turn = fireCount;
+    cout << "\n_____________________________";
+    cout << "\n\nTurn " << turn + 1;
+    cout << "\nDisplaying the current board state\n\n";
+}
+
+void fireShip(char playerName)
+{
+    char X, Y;
+    int fireCount = 0;
+    cout << "\nPlayer " << playerName << " enter coordinates to fire:";
+    cout << "\n____________________________________";
+    while (fireCount < 2)
+    {
+        displayBoardState(fireCount);
+        getCoordinates(X, Y);
+        cout << "\nYou fired at X: " << X << " and "
+             << " Y: " << Y;
+        fireCount++;
+    }
+}
+
+void beginGame()
+{
+    char playerName, X, Y;
+    int count = 0;
+    getPlayerName(playerName);
+
+    while (count < 2)
+    {
+        addShips(count);
+        if (count == 2)
+        {
+            enterToContinue();
+        }
+    }
+
+    fireShip(playerName);
+    enterToContinue();
+}
+
+void exitOut()
+{
+    cout << "\nGood Bye!!!\n";
+    exit(0);
+}
+
+void endGame()
+{
+    char response;
+    cout << "\n Are you sure you want to exit the game? (Y/N)\n";
+    cin >> response;
+    switch (response)
+    {
+    case 'Y':
+    case 'y':
+        exitOut();
+        break;
+    case 'N':
+    case 'n':
+    default:
+        printMainMenu();
+        break;
+    }
+}
+
 int displayMenu()
 {
     int response = 0;
@@ -80,7 +161,7 @@ int displayMenu()
     return response;
 }
 
-void printMenu()
+void printMainMenu()
 {
     switch (displayMenu())
     {
@@ -93,13 +174,13 @@ void printMenu()
     default:
         cout << "\n Sorry! Invalid Choice";
         // Loops the code if found invalid
-        printMenu();
+        printMainMenu();
         break;
     }
 }
 
 int main()
 {
-    printMenu();
+    printMainMenu();
     return 0;
 }
